7cc0f6bee6e3b32ac956ee74ca6061153fb67bd1
Rratic <Rratic@users.noreply.github.com>
2022-07-22 03:05:15 +0000

    deploy: f32b45c52965d570f2b073fc00f144a74ef17fdf

---
+++ extra/info.js
@@ -0,0 +1,21 @@
+const buildmessage=`built at Fri Jul 22 03:05:12 2022`
+const page_foot=`Powered by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> and its dependencies.`
+const tar_css=`css`
+const filesuffix=`.html`
+const menu=['',[`meta/Meta`,`about/关于`,`guide/使用指南`,],[`low/底层语法`,],[`core/内置内容`,`types/内置类型`,],[`base/Base内容`,],[`stdlib/标准库`,`about/关于`,],]
+const configpaths=        {'headroom': 'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.10.3/headroom.min',
+		'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min',
+		'headroom-jquery': 'https://cdnjs.cloudflare.com/ajax/libs/headroom/0.10.3/jQuery.headroom.min',
+		'katex': 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/katex.min',
+		'highlight': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min',
+        'hljs-julia': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/languages/julia.min',
+		'hljs-julia-repl': 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/languages/julia-repl.min',
+        'hljs-line-numbers': 'https://cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min'}
+        
+const configshim=        {"hljs-julia-repl": { "deps": ["highlight"] },
+		"hljs-julia": {"deps": ["highlight"]},
+        "hljs-line-numbers": { "deps": ["highlight"]},
+		"headroom-jquery": { "deps": [ "jquery", "headroom" ]}}
+        
+const hljs_languages=['julia', 'julia-repl']
+const main_requirement=['jquery', 'highlight', 'hljs-julia', 'hljs-julia-repl', 'hljs-line-numbers']
