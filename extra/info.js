d947b7525b8332fcda1f53d8ec21ebdd226ae759
Rratic <Rratic@users.noreply.github.com>
2022-07-23 08:32:01 +0000

    deploy: 5e38df487e13ee7179272c30f6ee83069fdd57cf

--- extra/info.js
+++ extra/info.js
@@ -1,4 +1,4 @@
-const buildmessage=`built at Fri Jul 22 10:10:13 2022`
+const buildmessage=`built at Sat Jul 23 08:31:57 2022`
 const page_foot=`Powered by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> and its dependencies.`
 const tar_css=`css`
 const filesuffix=`.html`
